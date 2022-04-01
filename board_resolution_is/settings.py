df3341dfdb524759b686548a15f6244e4089d298
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2022-04-02 07:02:17 +0800

    fix: Add default values for email

--- board_resolution_is/settings.py
+++ board_resolution_is/settings.py
@@ -179,10 +179,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['BRIS_EMAIL_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['BRIS_EMAIL_PASSWORD']
+EMAIL_HOST_USER = os.environ.get('BRIS_EMAIL_USERNAME', '')
+EMAIL_HOST_PASSWORD = os.environ.get('BRIS_EMAIL_PASSWORD', '')
 
-DEFAULT_FROM_EMAIL = os.environ['BRIS_EMAIL_USERNAME']
+DEFAULT_FROM_EMAIL = os.environ.get('BRIS_EMAIL_USERNAME', '')
 
 # Default Redirects
 
