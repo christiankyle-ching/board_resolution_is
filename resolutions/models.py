481a647176f1d89a8bad3828c18d51fd460b3731
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2022-04-04 16:05:24 +0800

    add: Initial Migrations

--- resolutions/models.py
+++ resolutions/models.py
@@ -1,3 +1,33 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from board_resolution_is import abstract_models
+
+_User = get_user_model()
+
+
+class Certificate(abstract_models.NoDeleteModel):
+    added_by = models.ForeignKey(_User, null=True, on_delete=models.SET_NULL)
+    added_date = models.DateTimeField(null=False, default=timezone.now)
+
+    @property
+    def resolutions(self):
+        return Resolution.objects.filter(certificate=self)
+
+    @property
+    def images(self):
+        return CertificateImage.objects.filter(certificate=self)
+
+
+class Resolution(abstract_models.NoDeleteModel):
+    certificate = models.ForeignKey(Certificate, on_delete=models.CASCADE)
+
+    number = models.CharField(max_length=50, blank=False, null=False)
+    title = models.TextField(blank=False, null=False)
+
+
+class CertificateImage(abstract_models.NoDeleteModel):
+    certificate = models.ForeignKey(Certificate, on_delete=models.CASCADE)
+
+    image = models.ImageField(upload_to="certificates/")
