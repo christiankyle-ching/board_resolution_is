88e9fdc5d2432a16cc716b0cbeb4de1992b197c6
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2022-05-05 11:10:13 +0800

    fix: register models in admin.site

--- resolutions/admin.py
+++ resolutions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from resolutions.models import Certificate, CertificateImage, Resolution
+
 # Register your models here.
+admin.site.register(Resolution)
+admin.site.register(Certificate)
+admin.site.register(CertificateImage)
